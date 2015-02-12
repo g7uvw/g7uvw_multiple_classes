360e8dc2dac803d160da3531c490e6287b6e6263
David <dave@webshed.org>
2015-02-12 20:19:54 +0000

    Working, two tests in main now

--- main.cpp
+++ main.cpp
@@ -7,10 +7,17 @@
 int main(int argc, const char * argv[]) {
     
     FirstClass fc;
+    FirstClass fc2;
     
     fc.loaddata();
     fc.print();
     fc.printdata2();
+    
+    fc2.loaddata();
+    fc2.dt.xsize *= 2;
+    fc2.dt.voltage /= 2.1;
+    fc2.print();
+    fc2.printdata2();
     
     
     return 0;
